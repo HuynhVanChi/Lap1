a392369a7338db71e1546af5f29704ec7b6f2448
nguyen van a <vana@example.com>
2024-03-15 11:14:21 +0700

    up

---
+++ Lap1/bai2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Dien so vao R=";
+    int R;
+    cin >> R;
+    int pi=3.141592654;
+    int P=2*pi*R;
+    cout << "Phep tinh ra:" << P;
+
+    return 0;
+}
