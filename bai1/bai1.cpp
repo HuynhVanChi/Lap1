037957da768be0363d097cfa1a3bc78b13f45c01
nguyen van a <vana@example.com>
2024-03-15 08:40:21 +0700

    trai nghiem lan dau

---
+++ bai1/bai1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Dien so vao a=";
+    int a;
+    cin >> a;
+    cout << "Dien so vao b=";
+    int b;
+    cin >> b;
+    cout << "Dien so vao c=";
+    int c;
+    cin >> c;
+    int d = a + b + c;
+    cout << "Phep tinh ra:" << d;
+
+    return 0;
+}
